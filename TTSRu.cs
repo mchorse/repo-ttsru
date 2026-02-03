697208a3452cb793ed5d6ead18c4a76884fc1f97
McHorse <mchorselessrider@gmail.com>
2026-02-03 14:14:48 +0000

    Initial commit v1.0.1

---
+++ TTSRu.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace TTSRu;
+
+[BepInPlugin("McHorse.TTSRu", "TTSRu", "1.0.1")]
+public class TTSRu : BaseUnityPlugin
+{
+    public static readonly Dictionary<char, string> TOKENS = new()
+    {
+        ['А'] = "AA",  ['а'] = "aa",
+        ['Б'] = "B",   ['б'] = "b",
+        ['В'] = "V",   ['в'] = "v",
+        ['Г'] = "G",   ['г'] = "g",
+        ['Д'] = "D",   ['д'] = "d",
+        ['Е'] = "EH",  ['е'] = "eh",
+        ['Ё'] = "YO",  ['ё'] = "yo",
+        ['Ж'] = "ZH",  ['ж'] = "zh",
+        ['З'] = "Z",   ['з'] = "z",
+        ['И'] = "EE",  ['и'] = "ee",
+        ['Й'] = "Y",   ['й'] = "y",
+        ['К'] = "K",   ['к'] = "k",
+        ['Л'] = "L",   ['л'] = "l",
+        ['М'] = "M",   ['м'] = "m",
+        ['Н'] = "N",   ['н'] = "n",
+        ['О'] = "OH",  ['о'] = "oh",
+        ['П'] = "P",   ['п'] = "p",
+        ['Р'] = "R",   ['р'] = "r",
+        ['С'] = "S",   ['с'] = "s",
+        ['Т'] = "T",   ['т'] = "t",
+        ['У'] = "OO",  ['у'] = "oo",
+        ['Ф'] = "F",   ['ф'] = "f",
+        ['Х'] = "H",   ['х'] = "h",
+        ['Ц'] = "TS",  ['ц'] = "ts",
+        ['Ч'] = "CH",  ['ч'] = "ch",
+        ['Ш'] = "SS",  ['ш'] = "sh",
+        ['Щ'] = "SH",  ['щ'] = "sh",
+        ['Ъ'] = "",    ['ъ'] = "",
+        ['Ы'] = "EEH", ['ы'] = "eeh",
+        ['Ь'] = "",    ['ь'] = "",
+        ['Э'] = "AY",  ['э'] = "ay",
+        ['Ю'] = "OO",  ['ю'] = "oo",
+        ['Я'] = "YA",  ['я'] = "ya",
+    };
+    
+    internal static TTSRu Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger => Instance._logger;
+    private ManualLogSource _logger => base.Logger;
+    internal Harmony? Harmony { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        
+        // Prevent the plugin from being deleted
+        this.gameObject.transform.parent = null;
+        this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        Patch();
+
+        Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+    }
+
+    internal void Patch()
+    {
+        Harmony ??= new Harmony(Info.Metadata.GUID);
+        Harmony.PatchAll();
+    }
+
+    internal void Unpatch()
+    {
+        Harmony?.UnpatchSelf();
+    }
+}
---
+++ TTSVoicePatch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace TTSRu;
+
+[HarmonyPatch(typeof(PlayerAvatar))]
+static class TTSVoicePatch
+{
+    [HarmonyPrefix, HarmonyPatch(nameof(PlayerAvatar.ChatMessageSend))]
+    private static void TranslateSpecialLetters_Prefix(ref string _message)
+    {
+        _message = ReplaceTokensNoCascade(_message, TTSRu.TOKENS);
+    }
+    
+    public static string ReplaceTokensNoCascade(string text, IReadOnlyDictionary<char, string> tokens)
+    {
+        if (string.IsNullOrEmpty(text) || tokens.Count == 0)
+        {
+            return text;
+        }
+
+        var sb = new StringBuilder(text.Length);
+
+        foreach (char ch in text)
+        {
+            if (tokens.TryGetValue(ch, out var rep)) sb.Append(rep);
+            else sb.Append(ch);
+        }
+
+        return sb.ToString();
+    }
+}
